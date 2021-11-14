509fdba2decc278c54e1f28ed9831d01a0fbf315
D1m0000n <demisrus5554@gmail.com>
2021-11-14 21:47:25 +0300

    Change app

--- src/test/java/SimpleCalcTest.java
+++ src/test/java/SimpleCalcTest.java
@@ -1,26 +1,15 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import junit.framework.TestCase;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+public class SimpleCalcTest extends TestCase {
+    Calculator calc;
 
-public class SimpleCalcTest {
-    Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
+    public void testSum() {
+        calc = new Calculator();
+        assertEquals(4, calc.sum(2, 2));
     }
 
-    @Test
-    @DisplayName("Simple multiplication")
-    void testMultiply() {
-        assertEquals(20, calculator.multiply(5, 4));
-    }
-
-    @Test
-    @DisplayName("Simple summarization")
-    void testSum() {
-        assertEquals(9, calculator.sum(5, 4));
+    public void testMult() {
+        calc = new Calculator();
+        assertEquals(4, calc.multiply(2, 2));
     }
 }
