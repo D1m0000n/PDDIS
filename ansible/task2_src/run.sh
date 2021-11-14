70ca7bd9d9523e887e5f69d4710b1f7901d2178d
D1m0000n <demisrus5554@gmail.com>
2021-11-14 20:42:14 +0300

    Add application

--- ansible/task2_src/run.sh
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/bash
-
-/usr/local/go/bin/go run /home/mftiedu/dnet/task2_src/echo_server.go &
-
-ep_pid=$!
-sleep 5
-echo $ep_pid >pid
--- ansible/task2_src/stop.sh
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/bash
-
-read ep_pid < pid
-kill -9 $ep_pid
-rm pid
